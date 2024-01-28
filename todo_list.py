3e95a1e7d5768d6e6f70b592eb37aceee7a9065d
GouravSharma99 <sharmagourav3699@gmail.com>
2024-01-28 17:49:00 +0530

    change remove comments

--- todo_list.py
+++ todo_list.py
@@ -8,30 +8,16 @@ while True:
     user_action = input("Enter add , show , edit , complete or exit : ").strip().lower()
 
     if user_action.startswith("add"):
-        todo = input("Enter the todo : ").capitalize() + "\n"
         todo = user_action[4:].capitalize() + "\n"
-        
-        file = open("todos.txt", "r")
-        todos = file.readlines()  #readlines method return a list 
-        file.close()
         
         todos = get_todos()
         
         todos.append(todo)
         
-        # todo_file = open("todos.txt", "w")
-        # todo_file.writelines(todos)
-        # todo_file.close()
-        
         write_todos(todos)
             
         
     elif user_action.startswith("show"):
-        
-        # file = open("todos.txt", "r")
-        # todos = file.readlines()
-        # file.close()
-        
         
         todos = get_todos()
         
